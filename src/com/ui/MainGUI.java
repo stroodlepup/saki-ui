237e9e5f4d2a43f3f717829e7da0bf31c6b33582
stroodlepup <stroodlepup@LAPPIE>
2017-10-19 17:40:38 +0800

    added Main GUI components

---
+++ src/com/ui/MainGUI.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ui;
+
+import com.ui.components.LineSegmentItem;
+import com.ui.frames.MainContainer;
+
+/**
+ *
+ * @author stroodlepup
+ */
+public class MainGUI {
+
+    public MainGUI(){
+        new MainContainer();
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    private MainContainer mainContainer;
+}
